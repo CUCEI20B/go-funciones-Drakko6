038cc69fb0c82ed14fe67461bd4b5f6378a68ce0
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-10-13 21:04:33 -0500

    Funciona algoritmo burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,33 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
+
+func Burbuja(s []int64) []int64 {
+	fmt.Println("Hola")
+	for i:=1; i <len(s); i++{
+		for j:=0; j<len(s)-i; j++{
+            if(s[j] > s[j+1]){
+				k := s[j+1]
+                s[j+1] = s[j]
+                s[j] = k;
+			}
+		}	
+	}	
+	return s    
 
 }
 
 func main()  {
+
+	s := []int64{1, -10, 90, 14, -100, -2}
+	fmt.Println(s)
+	s= Burbuja(s)
+	fmt.Println(s)
+
+
+
+
+
+
 	
 }
